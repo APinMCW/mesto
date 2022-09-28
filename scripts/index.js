a1409abfcbdb683cd8bf330562532b2913abdd29
Aleksander Panferov <panferov.a.a@yandex.ru>
2022-09-28 20:47:08 +0300

    feat: add popup-add-card

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,27 @@ const overlayEl = document.querySelector('.overlay');
 const openPopupButton = document.querySelector('.profile__button');
 const closePopupButton = overlayEl.querySelector('.popup__close');
 const saveButton = overlayEl.querySelector('.popup__save');
+const popupProfile = document.querySelector('.popup');
+
+function overlayClose () {
+    overlayEl.classList.remove('overlay_opened');
+};
+
+function overlayOpen () {
+    overlayEl.classList.add('overlay_opened');
+};
 
 openPopupButton.addEventListener('click', () => {
-    overlayEl.classList.add('overlay_opened');
+    overlayOpen ();
+    popupProfile.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
     // открыть попап и записать в интпуты значения из профиля
 })
 
 closePopupButton.addEventListener('click', () => {
-    overlayEl.classList.remove('overlay_opened');
+    overlayClose ();
+    popupProfile.classList.remove('popup_opened');
     nameInput.value = '';
     jobInput.value = '';
     // закрыть попап и очистить инпуты
@@ -28,9 +39,33 @@ function popupValue (event) {
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     // записать в профиль значения из инпутов
-    overlayEl.classList.remove('overlay_opened');
+    overlayClose ();
+    popupProfile.classList.remove('popup_opened');
     // закрыть попап
 }
 
 saveButton.addEventListener('click', popupValue);
 // сохранить данные из формы в профиль по клику сохранить и закрыть попап
+
+const openAddCardButton = document.querySelector('.profile__plus');
+const closeAddCardButton = overlayEl.querySelector('.popup-add-card__close');
+const saveAddCardButton = overlayEl.querySelector('.popup-add-card__save');
+const popupAddCard = overlayEl.querySelector('.popup-add-card');
+
+openAddCardButton.addEventListener('click', () => {
+    overlayOpen ();
+    popupAddCard.classList.add('popup-add-card_opened');
+});
+
+closeAddCardButton.addEventListener('click', () => {
+    overlayClose ();
+    popupAddCard.classList.remove('popup-add-card_opened');
+    addCardInputName.value = '';
+    addCardInputUrl.value = '';
+    // закрыть попап и очистить инпуты
+})
+
+let addCardInputName = popupAddCard.querySelector('.popup-add-card__input_data_name');
+let addCardInputUrl = popupAddCard.querySelector('.popup-add-card__input_data_url');
+let InputCardName = popupAddCard.querySelector('.popup-add-card__input_data_name');
+let InputCardUrl = popupAddCard.querySelector('.popup-add-card__input_data_url');

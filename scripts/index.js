9ce106cb396f77a31b20b5e56becf04642214f60
Aleksander Panferov <panferov.a.a@yandex.ru>
2022-09-30 00:12:42 +0300

    feat: add render massive card

--- scripts/index.js
+++ scripts/index.js
@@ -4,21 +4,12 @@ const closePopupButton = overlayEl.querySelector(".popup__close");
 const saveButton = overlayEl.querySelector(".popup__save");
 const popupProfile = document.querySelector(".popup");
 
-<<<<<<< HEAD
 function overlayClose() {
     overlayEl.classList.remove("overlay_opened");
 }
 
-function overlayOpen() {
-    overlayEl.classList.add("overlay_opened");
-=======
-function overlayClose () {
-    overlayEl.classList.remove('overlay_opened');
-}
-
 function overlayOpen () {
     overlayEl.classList.add('overlay_opened');
->>>>>>> feature/popup
 }
 
 openPopupButton.addEventListener("click", () => {
@@ -62,17 +53,11 @@ const closeAddCardButton = overlayEl.querySelector(".popup-add-card__close");
 const saveAddCardButton = overlayEl.querySelector(".popup-add-card__save");
 const popupAddCard = overlayEl.querySelector(".popup-add-card");
 
-<<<<<<< HEAD
-openAddCardButton.addEventListener("click", () => {
-    overlayOpen();
-    popupAddCard.classList.add("popup-add-card_opened");
-});
-=======
 openAddCardButton.addEventListener('click', () => {
     overlayOpen ();
     popupAddCard.classList.add('popup-add-card_opened');
+    saveAddCardButton.addEventListener('click', handleSaveCard);
 })
->>>>>>> feature/popup
 
 closeAddCardButton.addEventListener("click", () => {
     overlayClose();
@@ -82,35 +67,89 @@ closeAddCardButton.addEventListener("click", () => {
     // закрыть попап и очистить инпуты
 });
 
-<<<<<<< HEAD
-let addCardInputName = popupAddCard.querySelector(".popup-add-card__input_data_name");
-let addCardInputUrl = popupAddCard.querySelector(".popup-add-card__input_data_url");
-let InputCardName = popupAddCard.querySelector(".popup-add-card__input_data_name");
-let InputCardUrl = popupAddCard.querySelector(".popup-add-card__input_data_url");
 
+
+
+const addCardInputName = popupAddCard.querySelector(".popup-add-card__input_data_name");
+const addCardInputUrl = popupAddCard.querySelector(".popup-add-card__input_data_url");
 // Добавление новой карточки
-const elements = document.querySelector(".elements");
-const itemTemplate = document.querySelector(".elements-item-template");
 
-function renderElement(InputCardName, InputCardUrl) {
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+
+const elements = document.querySelector(".elements");
+const itemTemplate = document.querySelector(".elements-item-template").content;
+const overlayImg = document.querySelector(".overlay-img");
+const popupImg = document.querySelector('.popup-img');
+
+
+function creatNewCard(value) {
     const newHtmlElement = itemTemplate.cloneNode(true); // клонируем ноду
     const header = newHtmlElement.querySelector(".elements__title");
-    header.textContent = InputCardName; // устанавливаем заголовок элемента
+    const image = newHtmlElement.querySelector(".elements__img");
 
-    newHtmlElement.querySelector(".elements__img").setAttribute("alt", InputCardName); // устанавливаем атрибут alt для картинки
-    newHtmlElement.querySelector(".elements__img").setAttribute("src", InputCardUrl); // устанавливаем атрибут src для картинки
+    header.textContent = value.name; // устанавливаем заголовок элемента
+    image.alt = value.name; // устанавливаем атрибут alt для картинки
+    image.src = value.link; // устанавливаем атрибут src для картинки
 
 
-    setListenersForItem(newHtmlElement); // назначаем листенеры внутри каждого элемента
-    elements.append(newHtmlElement); // вставляем в DOM
+    setListenersForElement(newHtmlElement); // назначаем листенеры внутри каждого элемента
+    // elements.appendChild(newHtmlElement); // вставляем в DOM
+    return newHtmlElement
+}
+
+const startCards = initialCards.map(creatNewCard);
+
+function render() {
+	elements.append(...startCards);
+}
+
+
+function handleSaveCard() {
+    const inputName = addCardInputName.value;
+    const inputLink = addCardInputUrl.value;
+    const newCard = [{
+        name: inputName,
+        link: inputLink
+    }];
+    newCard.forEach(creatNewCard);
+    elements.append(newCard);
 }
 
 function setListenersForElement(element) {
     const deleteButton = element.querySelector(".elements__trash");
-    deleteButton.addEventListener("click", handleDelete); // TODO передаем ссылку на функцию
+    deleteButton.addEventListener("click", handleDelete); // передаем ссылку на функцию
 
     const likeButton = element.querySelector(".elements__like");
     likeButton.addEventListener("click", handleLike);
+
+    const imgClick = element.querySelector(".elements__img");
+    imgClick.addEventListener('click', handleImg);
 }
 
 function handleDelete(event) {
@@ -122,14 +161,13 @@ function handleLike(event) {
     const likeButton = event.target.closest(".elements__like");
     likeButton.classList.toggle("elements__like_active");
 }
-=======
-function savePopupAddCard (addCardInputName, addCardInputUrl) {
-    const InputCardName = addCardInputName.value;
-    const InputCardUrl = addCardInputUrl.value;
+
+function handleImg(event) {
+    const imgClick = event.target.closest(".elements__img");
+    overlayImg.classList.add('overlay-img_opened');
+    popupImg.classList.add('popup-img_opened');
+    popupImg.src = 
+    imgClick
 }
 
-saveAddCardButton.addEventListener('click', render());
-
-let addCardInputName = popupAddCard.querySelector('.popup-add-card__input_data_name');
-let addCardInputUrl = popupAddCard.querySelector('.popup-add-card__input_data_url');
->>>>>>> feature/popup
+render();

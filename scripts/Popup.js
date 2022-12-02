1a00060a09d40988000545eee4a5e2fc12b3b1e1
Aleksander Panferov2 <panferov.a.a@yandex.ru>
2022-12-02 15:29:21 +0300

    feat: add listener close by esc from popup and separation note fonts and images

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -2,14 +2,17 @@ export class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
     this._buttonClose = this._popup.querySelector(".popup__close");
+    this._handleEscCloseRef = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popup.classList.add("popup_opened");
+    document.addEventListener("keydown", this._handleEscCloseRef);
   }
 
   close() {
     this._popup.classList.remove("popup_opened");
+    document.removeEventListener("keydown", this._handleEscCloseRef);
   }
 
   _handleEscClose() {
